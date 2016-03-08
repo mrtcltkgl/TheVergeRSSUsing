d5b18c4c45b47d286a0ea62240cf2d8d538e693c
GBGYP-7 <gbgyp-7@gbgyp-7-imac.local>
2016-03-08 18:42:56 +0200

    Initial Commit

---
+++ RssFeedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RssFeedDemo
+//
+//  Created by GBGYP-7 on 8.03.2016.
+//  Copyright © 2016 GBGYP-7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RssFeedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RssFeedDemo
+//
+//  Created by GBGYP-7 on 8.03.2016.
+//  Copyright © 2016 GBGYP-7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
